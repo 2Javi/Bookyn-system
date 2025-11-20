7741af3a85c98c7e1693bda3baf0a1d42d486a8e
Javier <javierigher3@gmail.com>
2025-11-20 13:01:27 -0500

    Testing to see if jose can download my view

--- JHtesting/app/src/main/java/com/example/jhtesting/MainActivity.kt
+++ JHtesting/app/src/main/java/com/example/jhtesting/MainActivity.kt
@@ -5,12 +5,14 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-
+import com.example.jhtesting.databinding.ActivityMainBinding
 class MainActivity : AppCompatActivity() {
+    private lateinit var b : ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        b = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(b.root)
 
     }
 }
